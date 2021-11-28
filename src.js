f60b836beb276bbf329ae4c2ed6bc11a8c9b72cd
Zulqarnain <76658396+kernel-loophole@users.noreply.github.com>
2021-11-28 05:27:14 -0500

    Add files via upload

---
+++ src.js
@@ -0,0 +1,12 @@
+importScripts("https://cdnjs.cloudflare.com/ajax/libs/typed.js/2.0.0/typed.js",
+    "https://cdn.jsdelivr.net/npm/typed.js@2.0.12",
+    "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js",
+    "https://cdnjs.cloudflare.com/ajax/libs/typed.js/2.0.0/typed.min.js.map",
+)
+
+var typed6 = new Typed('#typed6', {
+    strings: ['web devepoleer', 'student', 'free man'],
+    typeSpeed: 70,
+    backSpeed: 10,
+    loop: true
+});
